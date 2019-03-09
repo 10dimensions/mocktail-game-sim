fb68af846399a34e38740adc5389ce3972bb3d1d
prayaan <33066836+prayaan@users.noreply.github.com>
2019-03-09 12:03:22 +0530

    game_dat-init

---
+++ Assets/Scripts/SimData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimData : MonoBehaviour
+{   
+    private static SimData _instance;
+    public static SimData Instance
+    {
+        get{return _instance;}
+    }
+
+    void Awake() 
+    { 
+        if (_instance != null && _instance != this) 
+        { 
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    } 
+
+}
